515c5c98a8d9bfd463e4a7d68c365fc7a162ea7f
Ajay <ajay.baviskar88@gmail.com>
2025-09-09 15:32:55 +0530

    added diamond

--- models/Diamond.js
+++ models/Diamond.js
@@ -7,9 +7,7 @@ const DiamondSchema = new mongoose.Schema({
     Purity: { type: String, default: null },
     Discount: { type: String, default: null },
     Price: { type: String, default: null },
-    certificateNo: { type: String, unique: true, required: true },
 
 }, { timestamps: true });
-DiamondSchema.index({ certificateNo: 1 }, { unique: true });
 
 module.exports = mongoose.model('Diamond', DiamondSchema);
