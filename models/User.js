98c75e5b89ce244c5fd16bf355a19e55b21767a8
Ajay <ajay.baviskar88@gmail.com>
2025-09-09 17:26:49 +0530

    added

--- models/User.js
+++ models/User.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    mobile: { type: String, required: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['MOBILE', 'ADMIN'], default: 'ADMIN' }
-
-
-
+  name: { type: String, required: true },
+  email: { type: String, required: true },   // Not unique
+  mobile: { type: String, required: true },  // Not unique
+  password: { type: String, required: true },
+  role: { 
+    type: String, 
+    enum: ['MOBILE', 'ADMIN'], 
+    default: 'ADMIN' 
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
